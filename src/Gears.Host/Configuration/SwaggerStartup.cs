a62b8e994b4ea38dab7edf0f6e518da7632e7334
Andrii Rebrin <studmm@ukr.net>
2023-12-05 14:43:38 +0100

    add swagger

---
+++ src/Gears.Host/Configuration/SwaggerStartup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Gears.Host.Configuration;
+
+internal sealed class SwaggerSettings
+{
+    public bool IsEnabled { get; init; }
+}
+
+internal static class SwaggerStartup
+{
+    public static WebApplicationBuilder ConfigureSwaggerSettings(this WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<SwaggerSettings>(builder.Configuration.GetSection("Swagger"));
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder ConfigureSwaggerServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+
+    public static IApplicationBuilder ConfigureSwaggerMiddleware(this IApplicationBuilder app)
+    {
+        var options = app.ApplicationServices.GetRequiredService<IOptions<SwaggerSettings>>();
+        if (options.Value.IsEnabled)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        return app;
+    }
+}
--- src/Gears.Host/Program.cs
+++ src/Gears.Host/Program.cs
@@ -1,6 +1,33 @@
+using Gears.Host.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigureConfiguration();
+ConfigureServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+ConfigureMiddleware();
+ConfigureEndpoints();
 
 app.Run();
+
+void ConfigureConfiguration()
+{
+    builder.ConfigureSwaggerSettings();
+}
+
+void ConfigureServices()
+{
+    builder.ConfigureSwaggerServices();
+}
+
+void ConfigureMiddleware()
+{
+    app.ConfigureSwaggerMiddleware();
+}
+
+void ConfigureEndpoints()
+{
+    app.MapGet("/", () => "Hello World!");
+}
