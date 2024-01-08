7350d417f22a5ef2b9093ea63a3087cd6145f946
Andrii Rebrin <studmm@ukr.net>
2024-01-08 22:47:19 +0100

    add Change Password feature

---
+++ src/Gears.Application/Features/Account/ChangePassword.cs
@@ -0,0 +1,62 @@
+﻿namespace Gears.Application.Features.Account;
+
+using ChangePasswordResult = Results<Ok, BadRequest, NotFound>;
+
+public sealed record ChangePasswordRequest(
+    string Password,
+    string NewPassword
+);
+
+public sealed class ResetPasswordRequestValidator : Validator<ChangePasswordRequest>
+{
+    public ResetPasswordRequestValidator()
+    {
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage("Password is required");
+
+        RuleFor(x => x.NewPassword)
+            .NotEmpty()
+            .WithMessage("New password is required");
+    }
+}
+
+public sealed class ChangePassword(
+    IHttpContextAccessor httpContextAccessor,
+    UserManager<User> userManager
+)
+    : Endpoint<ChangePasswordRequest, ChangePasswordResult>
+{
+    public override void Configure()
+    {
+        Post("api/account/change-password");
+    }
+
+    public override async Task<ChangePasswordResult> ExecuteAsync(ChangePasswordRequest request, CancellationToken ct)
+    {
+        var userId = userManager.GetUserId(httpContextAccessor.HttpContext!.User);
+        var user = await userManager.FindByIdAsync(userId!);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        foreach (var passwordValidator in userManager.PasswordValidators)
+        {
+            var passwordValidationResult = await passwordValidator.ValidateAsync(userManager, user, request.Password);
+            if (passwordValidationResult != IdentityResult.Success)
+            {
+                return BadRequest();
+            }
+        }
+
+        var result = await userManager.ChangePasswordAsync(user, request.Password, request.NewPassword);
+        if (!result.Succeeded)
+        {
+            return BadRequest();
+        }
+
+        return Ok();
+    }
+}
