795b178d763560df7f31449a18ff038bc7ba9a6c
Andrii Rebrin <studmm@ukr.net>
2024-01-10 21:10:14 +0100

    get all users

--- src/Gears.Application/Features/Users/GetAllUsers.cs
+++ src/Gears.Application/Features/Users/GetAllUsers.cs
@@ -3,13 +3,18 @@
 public sealed record GetAllUsersRequest;
 
 public sealed record GetAllUsersResponse(
-    string Id,
+    string UserName,
     string Email,
     string FirstName,
-    string LastName);
+    string LastName,
+    string PhoneNumber,
+    bool IsActive);
 
 
-public sealed class GetAllUsers : Endpoint<GetAllUsersRequest, List<GetAllUsersResponse>>
+public sealed class GetAllUsers(
+    IApplicationDbContext db
+)
+    : Endpoint<GetAllUsersRequest, List<GetAllUsersResponse>>
 {
     public override void Configure()
     {
@@ -19,6 +24,18 @@ public sealed class GetAllUsers : Endpoint<GetAllUsersRequest, List<GetAllUsersR
 
     public override async Task HandleAsync(GetAllUsersRequest req, CancellationToken ct)
     {
-        await SendAsync([]);
+        var result = await db.Users.AsNoTracking()
+            .Where(x => x.UserName != Consts.Auth.RootUser)
+            .Select(x => new GetAllUsersResponse(
+                x.UserName,
+                x.Email,
+                x.FirstName,
+                x.LastName,
+                x.PhoneNumber,
+                x.IsActive)
+            )
+            .ToListAsync(ct);
+
+        await SendAsync(result);
     }
 }
