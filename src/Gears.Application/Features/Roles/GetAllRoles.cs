be0f5210492c17fb7a2cd73dc603992f94c6f562
Andrii Rebrin <studmm@ukr.net>
2024-01-11 09:57:16 +0100

    get all roles

---
+++ src/Gears.Application/Features/Roles/GetAllRoles.cs
@@ -0,0 +1,37 @@
+﻿namespace Gears.Application.Features.Roles;
+
+public sealed record GetAllRolesRequest;
+
+public sealed record GetAllRolesResponse(
+    string Id,
+    string Name,
+    string Description,
+    bool IsDeletable,
+    bool IsDefault
+);
+
+public sealed class GetAllRoles(
+    IApplicationDbContext db
+)
+    : Endpoint<GetAllRolesRequest, List<GetAllRolesResponse>>
+{
+    public override void Configure()
+    {
+        Get("api/roles");
+        AccessControl("Roles-GetAll", Apply.ToThisEndpoint);
+    }
+
+    public override async Task HandleAsync(GetAllRolesRequest req, CancellationToken ct)
+    {
+        var result = await db.Roles.AsNoTracking()
+            .Select(x => new GetAllRolesResponse(
+                x.Id,
+                x.Name,
+                x.Description,
+                x.IsDeletable,
+                x.IsDefault))
+            .ToListAsync(ct);
+
+        await SendAsync(result);
+    }
+}
--- src/Gears.Application/Features/Users/GetAllUsers.cs
+++ src/Gears.Application/Features/Users/GetAllUsers.cs
@@ -3,13 +3,13 @@
 public sealed record GetAllUsersRequest;
 
 public sealed record GetAllUsersResponse(
+    string Id,
     string UserName,
     string Email,
     string FirstName,
     string LastName,
     string PhoneNumber,
     bool IsActive);
-
 
 public sealed class GetAllUsers(
     IApplicationDbContext db
@@ -27,13 +27,13 @@ public sealed class GetAllUsers(
         var result = await db.Users.AsNoTracking()
             .Where(x => x.UserName != Consts.Auth.RootUser)
             .Select(x => new GetAllUsersResponse(
+                x.Id,
                 x.UserName,
                 x.Email,
                 x.FirstName,
                 x.LastName,
                 x.PhoneNumber,
-                x.IsActive)
-            )
+                x.IsActive))
             .ToListAsync(ct);
 
         await SendAsync(result);
