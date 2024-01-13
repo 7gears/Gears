9b0abf8f3cc05326c7475db78e176c56311397f7
Andrii Rebrin <studmm@ukr.net>
2024-01-13 11:48:52 +0100

    get all permissions feature

---
+++ src/Gears.Application/Features/Permissions/GetAllPermissions.cs
@@ -0,0 +1,27 @@
+﻿using Gears.Application.Auth;
+
+namespace Gears.Application.Features.Permissions;
+
+public sealed record GetAllPermissionsRequest;
+
+public sealed record GetAllPermissionsResponse(
+    string Code,
+    string Name
+);
+
+public sealed class GetAllPermissions : Endpoint<GetAllPermissionsRequest, List<GetAllPermissionsResponse>>
+{
+    public override void Configure()
+    {
+        Get("api/permissions");
+    }
+
+    public override async Task HandleAsync(GetAllPermissionsRequest request, CancellationToken ct)
+    {
+        var permissions = Allow.AllPermissions()
+            .Select(x => new GetAllPermissionsResponse(x.PermissionCode, x.PermissionName))
+            .ToList();
+
+        await SendAsync(permissions);
+    }
+}
